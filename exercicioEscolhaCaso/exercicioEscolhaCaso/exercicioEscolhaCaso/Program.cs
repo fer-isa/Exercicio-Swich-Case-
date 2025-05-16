4d4cea87b3c1790f2764cefbc6c22315f26516ea
Fernanda Isabelli Oliveira <109819886+fer-isa@users.noreply.github.com>
2025-05-16 14:56:35 -0300

    Add files via upload

---
+++ exercicioEscolhaCaso/exercicioEscolhaCaso/exercicioEscolhaCaso/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace exercicioEscolhaCaso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            escolhaCaso escolhaCaso = new escolhaCaso();
+
+            escolhaCaso.escolhaCasoExercicio();
+        }
+    }
+}
---
+++ exercicioEscolhaCaso/exercicioEscolhaCaso/exercicioEscolhaCaso/escolhaCaso.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioEscolhaCaso
+{
+    internal class escolhaCaso
+    {
+        public void escolhaCasoExercicio()
+
+        {
+
+            string mes;
+
+            Console.WriteLine("Digite o número do mes: ");
+            mes = Console.ReadLine();
+
+            switch (mes)
+            {
+                case "1":
+                    Console.WriteLine("O mês escolhido é Janeiro");
+                    break;
+
+                case "2":
+                    Console.WriteLine("O mês escolhido é Fevereiro:");
+                    break;
+
+                default:
+                    Console.WriteLine("O mês  solicitado é invalido");
+                    break;
+            }
+        }
+    }
+ }
